6a621eab84efd31937a20672e630b1ce234d4483
GregoryBrinkman <gregorylucasbrinkman@siu.edu>
2017-12-23 15:34:28 -0600

    Angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { WeightComponent } from './weight/weight.component';
+import { DietComponent } from './diet/diet.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+
+const appRoutes: Routes = [
+  { path: 'weight', component: WeightComponent },
+  { path: 'diet', component: DietComponent },
+  { path: 'schedule', component: ScheduleComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    WeightComponent,
+    DietComponent,
+    ScheduleComponent
   ],
   imports: [
-    BrowserModule
+  BrowserModule,
+  RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent, NavComponent]
